2bc6f43edc51fbd9a901eb7f6feef7af71af0580
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2023-07-09 21:01:14 +0700

    update springdoc and fix bug

---
+++ src/driver/src/main/java/cabo/backend/driver/config/SpringDocConfig.java
@@ -0,0 +1,38 @@
+package cabo.backend.driver.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringDocConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI driverOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Driver")
+                        .description("Spring Boot REST API Documentation")
+                        .version("1")
+                        .contact(new Contact()
+                                .name("Nguyen Thien Phu")
+                                .email("ntphu1206@gmail.com")
+                        )
+                        .license(new License().name("License of API"))
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi driverApi() {
+        return GroupedOpenApi.builder()
+                .addOpenApiCustomiser(openApi -> openApi.info(driverOpenAPI().getInfo()))
+                .group("Driver")
+                .packagesToScan("cabo.backend.driver.controller")
+                .build();
+    }
+}
